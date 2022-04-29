b89841d72d52f3e39cbab3cf846eaa9c90d66b6b
Mitchell Paff <mpaff@linz.govt.nz>
2022-04-29 11:47:04 +0100

    get script working

--- main.py
+++ main.py
@@ -4,29 +4,28 @@ from pystac.item import Item
 from pystac.layout import HrefLayoutStrategy
 from pystac import read_file
 
+from pathlib import Path
+
+
 class GeostoreSTACLayoutStrategy(HrefLayoutStrategy):
     def get_catalog_href(self, cat: Catalog, parent_dir: str, is_root: bool) -> str:
         return str(cat.get_self_href())
 
-    def get_collection_href(self, col: Collection, parent_dir: str, is_root: bool) -> str:
+    def get_collection_href(
+        self, col: Collection, parent_dir: str, is_root: bool
+    ) -> str:
         assert not is_root
         return str(col.get_self_href())
 
     def get_item_href(self, item: Item, parent_dir: str) -> str:
         return str(item.get_self_href())
 
-"""Handle writing a new dataset version to the dataset catalog"""
 
+parent_catalog = Catalog.from_file("example/parent_catalog.json")
 
+child_catalog = read_file(f"example/test_dataset_1/catalog.json")
+parent_catalog.add_child(child=child_catalog, strategy=GeostoreSTACLayoutStrategy())
 
-root_catalog = Catalog.from_file(
-    "parent_catalog.json"
-)
-
-dataset_version_metadata = read_file(f"test_dataset_1/catalog.json")
-dataset_catalog.add_child(child=dataset_version_metadata, strategy=GeostoreSTACLayoutStrategy())
-
-dataset_catalog.normalize_hrefs(
-    f"{storage_bucket_path}/{dataset_prefix}", strategy=GeostoreSTACLayoutStrategy()
-)
-dataset_catalog.save(catalog_type=CatalogType.SELF_CONTAINED)
+path = str(Path(__file__).parent.absolute().joinpath("example"))
+parent_catalog.normalize_hrefs(path, strategy=GeostoreSTACLayoutStrategy())
+parent_catalog.save(catalog_type=CatalogType.SELF_CONTAINED)
